a3073d8af75f6eb9e98fdf00db02c0d41d52309f
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-10-23 19:38:19 +0530

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+import json
+import re
+import base64
+
+dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
 
 # Create your views here.
 def home(request):
     context = {
-        "title":"CaptureCam | Home"
+        "title":"CaptureCam | Home",
     }
     return render(request,'index.html',context)
+
+def file1(request):
+    context = {
+        "title":"CaptureCam | Home",
+    }
+    return render(request,'file.html',context)
+
+def captureImage(request):
+    dict = {}
+    context = {
+        "title":"HOME",
+    }
+    if request.method == 'POST':
+        image = request.POST.get('image')
+        # imageData = dataUrlPattern.match(image).group(2)
+        # ImageData = base64.b64decode(imageData)
+        # print(ImageData)
+    return HttpResponse("DONE")
