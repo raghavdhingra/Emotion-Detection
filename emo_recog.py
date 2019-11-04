058099c2bb73ff1b446b0b9d3827804c1c41f445
iamchiragsharma <chiragsharmagtbit@outlook.com>
2019-11-04 23:43:34 +0530

    Fixed Result Forever

--- emo_recog.py
+++ emo_recog.py
@@ -30,29 +30,32 @@ def face_detector(img):
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_gray = cv2.resize(roi_gray, (48, 48), interpolation = cv2.INTER_AREA)
-        print(roi_gray.shape)
+
         allfaces.append(roi_gray)
         rects.append((x,w,y,h))
     return rects, allfaces, img
 
-img = cv2.imread("test1.jpeg")
-rects, faces, image = face_detector(img)
 
-i = 0
-for face in faces:
-    print(face.shape)
-    print(face)
-    roi = face.astype("float") / 255.0
-    roi = img_to_array(roi)
-    roi = np.expand_dims(roi, axis=0)
+def emo_recog_save():
+	img = cv2.imread("test1.jpeg")
+	rects, faces, image = face_detector(img)
+	i = 0
+	for face in faces:
+	    print(face.shape)
+	    print(face)
+	    roi = face.astype("float") / 255.0
+	    roi = img_to_array(roi)
+	    roi = np.expand_dims(roi, axis=0)
 
-    # make a prediction on the ROI, then lookup the class
-    preds = classifier.predict(roi)[0]
-    label = class_labels[preds.argmax()]   
+	    # make a prediction on the ROI, then lookup the class
+	    preds = classifier.predict(roi)[0]
+	    label = class_labels[preds.argmax()]   
 
-    #Overlay our detected emotion on our pic
-    label_position = (rects[i][0] + int((rects[i][1]/2)), abs(rects[i][2] - 10))
-    i =+ 1
-    cv2.putText(image, label, label_position , cv2.FONT_HERSHEY_SIMPLEX,1, (0,255,0), 2)
-    
-cv2.imwrite("result.jpeg", image)
+	    #Overlay our detected emotion on our pic
+	    label_position = (rects[i][0] + int((rects[i][1]/2)), abs(rects[i][2] - 10))
+	    i =+ 1
+	    cv2.putText(image, label, label_position , cv2.FONT_HERSHEY_SIMPLEX,1, (0,255,0), 2)
+	    
+	cv2.imwrite("result.jpeg", image)
+
+
